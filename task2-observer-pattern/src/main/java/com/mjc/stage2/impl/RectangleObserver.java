3e6ba341a20694dd5f774aa98d52bc1d23363aa8
Artem Pasko <guramibox777@gmail.com>
2022-12-01 10:41:24 +0300

    add observer pattern

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,30 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        RectangleWarehouse rectangleWarehouse = RectangleWarehouse.getInstance();
+        Rectangle rectangle = event.getSource();
+        RectangleValues rectangleValues = rectangleWarehouse.get(rectangle.getId());
+        if(rectangleValues != null) {
+            rectangleValues.setPerimeter(perimeter(rectangle));
+            rectangleValues.setSquare(square(rectangle));
+        } else {
+            rectangleWarehouse.put(rectangle.getId(), new RectangleValues(perimeter(rectangle), square(rectangle)));
+        }
+    }
+
+    private double perimeter(Rectangle rectangle) {
+        return rectangle.getSideA() * 2 + rectangle.getSideB() * 2;
+    }
+
+    private double square(Rectangle rectangle) {
+        return rectangle.getSideA() * rectangle.getSideB();
+    }
 }
